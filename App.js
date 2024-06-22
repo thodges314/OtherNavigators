413b78b784c3abe6d5d43c4900d8e6b498a5b177
Thomas Hodges <thodges@javascriptonline.org>
2024-06-21 21:43:53 -0700

    basic tab conversion

--- App.js
+++ App.js
@@ -1,46 +1,21 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
 import { Ionicons } from '@expo/vector-icons';
 
-const Drawer = createDrawerNavigator();
+// const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#3c0a6b',
-          },
-          headerTintColor: 'white',
-          drawerActiveBackgroundColor: '#f0e1ff',
-          drawerActiveTintColor: '#3c0a6b',
-        }}
-      >
-        <Drawer.Screen
-          name="Welcome"
-          component={WelcomeScreen}
-          options={{
-            drawerLabel: 'Welcome Screen',
-            drawerIcon: ({ color, size }) => (
-              <Ionicons name="home" size={size} color={color} />
-            ),
-          }}
-        />
-        <Drawer.Screen
-          name="User"
-          component={UserScreen}
-          options={{
-            drawerLabel: 'User Screen',
-            drawerIcon: ({ color, size }) => (
-              <Ionicons name="person" size={size} color={color} />
-            ),
-          }}
-        />
-      </Drawer.Navigator>
+      <BottomTab.Navigator>
+        <BottomTab.Screen name="Welcome" component={WelcomeScreen} />
+        <BottomTab.Screen name="User" component={UserScreen} />
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
