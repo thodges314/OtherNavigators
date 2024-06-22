38ab9b870693da64a66d89a7a2d9eedd90c90bcf
Thomas Hodges <thodges@javascriptonline.org>
2024-06-21 20:49:08 -0700

    drawer navigation basics works

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
-import { Text } from 'react-native';
 import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen name="User" component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
